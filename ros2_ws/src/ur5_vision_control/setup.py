b2f58a2f29d1ef29cfc532300e61fbab168901e5
kubolsan <kuba.stetkiewicz@gmail.com>
2025-12-01 16:25:54 +0100

    Setup and package mod

--- ros2_ws/src/ur5_vision_control/setup.py
+++ ros2_ws/src/ur5_vision_control/setup.py
@@ -1,29 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'ur5_vision_control'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/ur5_vision_control.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='marco',
-    maintainer_email='marco@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='Vision control of UR5 using camera clicks and ArUco markers.',
+    license='MIT',
     entry_points={
         'console_scripts': [
+            'click_controller_node = ur5_vision_control.click_controller_node:main',
+            'aruco_controller_node = ur5_vision_control.aruco_controller_node:main',
         ],
     },
 )
