6be12bd21f5f9d0ef7771478932185626e4bdf3b
Marcosek21 <schneider.marco121@gmail.com>
2025-12-01 14:56:10 -0100

    Usb_cam test

---
+++ ros2_ws/install/ur5_vision_control/lib/python3.10/site-packages/ur5_vision_control/click_controller_node.py
@@ -0,0 +1,17 @@
+import rclpy
+from rclpy.node import Node
+
+class ClickControllerNode(Node):
+    def __init__(self):
+        super().__init__('click_controller_node')
+        self.get_logger().info("ClickControllerNode started (empty).")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ClickControllerNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
