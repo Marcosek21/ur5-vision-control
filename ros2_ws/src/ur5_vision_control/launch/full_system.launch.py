d993c86c05b9f1bd10d9ce522b4a25686a369b5e
Marcosek21 <schneider.marco121@gmail.com>
2025-12-04 18:39:26 +0100

    Launch update

--- ros2_ws/src/ur5_vision_control/launch/full_system.launch.py
+++ ros2_ws/src/ur5_vision_control/launch/full_system.launch.py
@@ -1,12 +1,14 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import os
 
+from launch_ros.actions import Node
+
+import os
 from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
 
