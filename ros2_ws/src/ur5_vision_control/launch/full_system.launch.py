a1bf11a5c07257a3d29e37e09d313f2e6743b126
Marcosek21 <schneider.marco121@gmail.com>
2025-12-04 18:30:04 +0100

    Updated launch file

---
+++ ros2_ws/src/ur5_vision_control/launch/full_system.launch.py
@@ -0,0 +1,71 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    # Parametr: czy użyć ArUco czy klikania
+    use_aruco = LaunchConfiguration('use_aruco')
+
+    declare_use_aruco = DeclareLaunchArgument(
+        'use_aruco',
+        default_value='true',
+        description='If true → start aruco_controller_node, else click_controller_node'
+    )
+
+    # Ścieżka do ur_robot_driver launch
+    ur_launch_file = os.path.join(
+        get_package_share_directory('ur_robot_driver'),
+        'launch',
+        'ur_control.launch.py'
+    )
+
+    # Node kamery
+    usb_cam_node = Node(
+        package='usb_cam',
+        executable='usb_cam_node_exe',
+        name='usb_cam',
+        output='screen'
+    )
+
+    # Node klikowy
+    click_node = Node(
+        package='ur5_vision_control',
+        executable='click_controller_node',
+        name='click_controller_node',
+        output='screen',
+        condition=IfCondition(PythonExpression(['not ', use_aruco]))
+    )
+
+    # Node ArUco
+    aruco_node = Node(
+        package='ur5_vision_control',
+        executable='aruco_controller_node',
+        name='aruco_controller_node',
+        output='screen',
+        condition=IfCondition(use_aruco)
+    )
+
+    # Sterownik UR5
+    ur_driver = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(ur_launch_file),
+        launch_arguments={
+            'ur_type': 'ur5',
+            'robot_ip': '192.168.56.101',
+            'launch_rviz': 'true'
+        }.items()
+    )
+
+    return LaunchDescription([
+        declare_use_aruco,
+        usb_cam_node,
+        click_node,
+        aruco_node,
+        ur_driver
+    ])
