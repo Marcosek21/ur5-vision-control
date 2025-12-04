f5050d70b532519eebfba0266eab6dc88f3ba1eb
Marcosek21 <schneider.marco121@gmail.com>
2025-12-04 18:43:36 +0100

    Launch update

--- ros2_ws/src/ur5_vision_control/launch/full_system.launch.py
+++ ros2_ws/src/ur5_vision_control/launch/full_system.launch.py
@@ -60,7 +60,7 @@ def generate_launch_description():
         launch_arguments={
             'ur_type': 'ur5',
             'robot_ip': '192.168.56.101',
-            'launch_rviz': 'true'
+            'launch_rviz': 'True'
         }.items()
     )
 
